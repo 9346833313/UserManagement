060c407fe49eed1302025729e06df9c75e7287e3
9346833313 <kanagalaanitha08@gmail.com>
2024-07-10 11:01:17 +0530

    added properties

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,18 @@
 package com.example.demo.controller;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping
+@RestController
 public class UserController {
 
+	@PostMapping
+	public ResponseEntity<?> createUser() {
+		return null;
+
+	}
 }
