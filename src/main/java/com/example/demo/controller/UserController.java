7674a3ef236405b567ef61073ec3551dbb23bd28
sreekanthvellulli <sreekanth.vellulli@spsoft.in>
2024-07-10 17:25:34 +0530

    added save method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,7 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
-
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -18,7 +18,10 @@ public class UserController {
 	}
 	
 	
-	
+	@GetMapping
+	public ResponseEntity<?> getMethod() {
+		return null;
 
+	}
 
 }
