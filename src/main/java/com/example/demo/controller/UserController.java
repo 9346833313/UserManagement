dfbfca69144a16267e90e555167f830e98260571
9346833313 <kanagalaanitha08@gmail.com>
2024-07-10 16:05:41 +0530

    created the methods in controller

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,10 +1,12 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
 
 @RequestMapping
 @RestController
@@ -14,5 +16,10 @@ public class UserController {
 	public ResponseEntity<?> createUser() {
 		return null;
 
+	}
+	
+	@GetMapping
+	public ResponseEntity<?> getAllUsers(){
+		return null;
 	}
 }
