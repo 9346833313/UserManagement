d6f35839efbb253149d5e07c430a8b01c3e05e52
9346833313 <kanagalaanitha08@gmail.com>
2024-07-11 10:12:25 +0530

    delete

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -14,21 +14,6 @@ public class UserController {
 	public ResponseEntity<?> delete(){
 		return null;
 	}
-<<<<<<< HEAD
-=======
 
-	@GetMapping
-	public ResponseEntity<?> getAllUsers() {
-		return null;
-
-	}
-
-	@GetMapping
-	public ResponseEntity<?> getById() {
-
-		return null;
-
-	}
 	
->>>>>>> a48cd38a910ad4e6a37ef6d51e52b310349d229f
 }
