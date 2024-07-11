a48cd38a910ad4e6a37ef6d51e52b310349d229f
sreekanthvellulli <sreekanth.vellulli@spsoft.in>
2024-07-11 09:59:45 +0530

    delete method

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,7 +1,6 @@
 package com.example.demo.controller;
 
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
@@ -31,11 +30,4 @@ public class UserController {
 
 	}
 	
-	@DeleteMapping
-	public ResponseEntity<?> deleteById() {
-
-		return null;
-
-	}
-
 }
